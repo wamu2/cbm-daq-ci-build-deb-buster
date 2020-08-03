16778a44a22ef35940c9865597e68147369ca7d0
wamu2 <wamu@retro11.de>
2020-08-03 16:34:03 +0200

    install msgpack cpp

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ set -e
 set -x
 
 # define versions
-#   gtest    1.10
 export VTAG_GTEST=tags/release-1.10.0
+export VTAG_MSGPACK_CPP=70912ff
 
 # OS version info
 grep VERSION /etc/os-release
@@ -20,6 +20,17 @@ mkdir build
 cd build
 cmake ..
 make
+make install
+
+# install msgpack cpp
+cd /tmp
+git clone https://github.com/msgpack/msgpack-c.git
+cd msgpack-c
+git checkout cpp_master
+git checkout $VTAG_MSGPACK_CPP
+mkdir build
+cd build
+cmake -DMSGPACK_BUILD_TESTS=OFF -DMSGPACK_BUILD_EXAMPLES=OFF ..
 make install
 
 # drop tmp files
